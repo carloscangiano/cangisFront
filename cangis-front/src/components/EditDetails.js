a7caab64f42dc73e25c4f5baf29fe02b174ac497
Carlos Cangiano <carlos.cangiano@ross.com.ar>
2021-07-23 13:09:02 -0300

    Navbar Buttons

--- cangis-front/src/components/EditDetails.js
+++ cangis-front/src/components/EditDetails.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
+import MyButton from '../util/MyButton';
 // Redux stuff
 import { connect } from 'react-redux';
 import { editUserDetails } from '../redux/actions/userActions';
@@ -67,11 +68,9 @@ class EditDetails extends Component {
         const { classes } = this.props;
         return (
             <Fragment>
-                <Tooltip title="Edit details" placement="top">
-                    <IconButton onClick={this.handleOpen} className={classes.button}>
-                        <EditIcon color="primary"/>
-                    </IconButton>
-                </Tooltip>
+                <MyButton tip="Edit Details" onClick={this.handleOpen} btnClassName={classes.button}>
+                    <EditIcon color="primary" />
+                </MyButton>
                 <Dialog 
                     open={this.state.open}
                     onClose={this.handleClose}
